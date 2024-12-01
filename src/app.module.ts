fc721d9b70014e917096d45f07ca351afd0d5308
igordg777 <igordg@mail.ru>
2024-12-01 03:11:07 +0300

    users module, controller, service from cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from './users/users.module';
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
     imports: [
         SequelizeModule.forRoot({
             dialect: 'postgres',
@@ -17,6 +16,7 @@ import { SequelizeModule } from "@nestjs/sequelize";
             models: [],
             autoLoadModels: true
         }),
+        UsersModule,
     ],
 })
 
