8deb1f8bddcafad5857f0007e83c58cd0cbb0ed2
igordg777 <igordg@mail.ru>
2024-12-04 06:10:13 +0300

    add User service, controller, dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,4 @@
+export class createUserDTO {
+    readonly email: string;
+    readonly password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { createUserDTO } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private userService: UsersService) { }
+
+    @Post()
+    create(@Body() userDto: createUserDTO) {
+        return this.userService.createUser(userDto)
+    }
+
+    @Get()
+    getAll() {
+        return this.userService.getAllUsers();
+    }
+}
